6018320205d3cfb63e9d0fc85285e23f42591461
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-05-26 22:59:52 -0300

    Initial commit

---
+++ 01-Dominando-a-Linguagem-Java/02-Tipos-de-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.33;
+        System.out.println(salarioMinimo);
+
+        /*
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal;
+        */
+    }
+}
