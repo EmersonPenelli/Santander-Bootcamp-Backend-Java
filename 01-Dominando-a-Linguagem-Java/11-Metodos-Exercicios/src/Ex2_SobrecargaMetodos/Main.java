c08bda1e4d8e9fb69686b9fb88eee07be60edf4c
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-05 22:40:59 -0300

    Initial commit

---
+++ 01-Dominando-a-Linguagem-Java/11-Metodos-Exercicios/src/Ex2_SobrecargaMetodos/Main.java
@@ -0,0 +1,15 @@
+package Ex2_SobrecargaMetodos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercí­cio quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+        System.out.println();
+        
+    }
+}
