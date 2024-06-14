24fe32c9249509ac028339ce6b97858fc2f4502f
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-13 22:33:18 -0300

    Initial commit

---
+++ 02-POO/Pacotes-e-Visibilidade/src/one/digitalinnovation/oo/outropacote/ExemploPacotes2.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.oo.outropacote;
+
+import one.digitalinnovation.oo.outropacote.ExemploPacotes1;
+
+public class ExemploPacotes2 {
+
+    ExemploPacotes1 exemploPacotes1; 
+}
